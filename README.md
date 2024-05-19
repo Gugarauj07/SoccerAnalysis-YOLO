f5bdce6f8f099689eb541164ac591c9f69ac3dcf
Gugarauj07 <gugaraujo01@gmail.com>
2024-05-18 22:40:03 -0400

    downloaded dataset

---
+++ README.md
@@ -0,0 +1,7 @@
+# Football Analysis Project
+
+## Introduction
+The goal of this project is to detect and track players, referees, and footballs in a video using YOLO, a object detection model. 
+
+### DATASET:
+https://universe.roboflow.com/roboflow-jvuqo/football-players-detection-3zvbc/dataset/1
--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-results = model.predict('input_videos/08fd33_4.mp4',save=True)
+results = model.predict('input_videos/023708_png.rf.b650bc63661fb413516db4ca2cc27872.jpg',save=True)
 print(results[0])
 print('=====================================')
 for box in results[0].boxes:
