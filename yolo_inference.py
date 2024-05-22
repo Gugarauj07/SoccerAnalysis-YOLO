7325710bfe0fe5509f16e726da410d619b6e1173
Gugarauj07 <gugaraujo01@gmail.com>
2024-05-21 21:35:34 -0400

    Initial Inference

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO 
 
-model = YOLO('yolov8x')
+model = YOLO(r'training\runs\detect\train4\weights\best.pt')
 
-results = model.predict('input_videos/023708_png.rf.b650bc63661fb413516db4ca2cc27872.jpg',save=True)
-print(results[0])
-print('=====================================')
-for box in results[0].boxes:
-    print(box)
+results = model.predict(r'training\CaptchaDetection-1\CaptchaDetection-1\test\images\889120_png.rf.2dcf76e8028b43ac24b8cb31c4edbcf2.jpg')
+print(results.tojson())
+# Process results list
